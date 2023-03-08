8ae05faa9746b1081e1d6b5e2ac7b2709be444b5
Aabhushan47 <khanalaabushan10@gmail.com>
2023-03-08 09:17:52 +0545

    me

---
+++ src/pages/Room/index.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import { useParams } from "react-router-dom"
+import { ZegoUIKitPrebuilt } from '@zegocloud/zego-uikit-prebuilt'
+
+const RoomPage = () => {
+    const { roomId } = useParams();      
+
+    const myMeeting = async (element) => {
+        const appID = 1884276449;
+        const serverSecret = "77928e1a1b616e46cf2b32e49c0872b9";
+        const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
+            appID,
+            serverSecret,
+            roomId,
+            Date.now().toString(),
+            'Aabhushan'
+        );
+
+        const zc = ZegoUIKitPrebuilt.create(kitToken);
+        zc.joinRoom({
+            container: element,
+            sharedLinks: [
+                {
+                    name: 'CopyLink',
+                    url: `http://localhost:3000/room/${roomId}`
+                }
+            ],
+            scenario: {
+                mode: ZegoUIKitPrebuilt.OneONoneCall,
+            },
+            showRoomTimer:true
+        });
+
+    }
+
+    return (
+    <div
+      className="myCallContainer"
+      ref={myMeeting}
+      style={{ width: '100vw', height: '100vh' }}
+    ></div>
+    )
+}
+
+export default RoomPage
